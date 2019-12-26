dbef0ac69ffa16cacd52df0196826b6275eb2044
Amogh Dambal <amdam2013@gmail.com>
2019-12-25 22:39:57 -0600

    added support for ELO and FO sourced data
    added more methods to sanitize and clean dataframes scraped from online datasets

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,28 +1,38 @@
 # python 3.6
 # code to clean and process
 # CSV data
-# supported functions
+# supported functions include
 import re
 
 
-def clean(dataset):
+# PFF
+def clean(dataset, source):
 	"""
 	sanitizes a given dataset by removing erroneous
 	or corrupted data
 	:param dataset: array of pandas.DataFrame objects
 	:return: None
 	"""
-	for df in dataset:
-		# remove NaN values
-		df.dropna(inplace=True)
 
-		# only consider qb's with 100 attempts
-		df.drop(df[df.Att < 100].index, inplace=True)
+	if source is 'PFF':
+		for df in dataset:
+			# remove NaN values
+			df.dropna(inplace=True)
 
-		# remove extra carets from player name
-		df.Player = [remove_carets(p) for p in df.Player]
+			# only consider qb's with 100 attempts
+			df.drop(df[df.Att < 100].index, inplace=True)
+
+			# remove extra carets from player name
+			df.Player = [remove_carets(p) for p in df.Player]
+	elif source is 'FO':
+		for df in dataset:
+			df.drop(columns=['Rk', 'Rk.1', 'Rk.2', 'Rk.3'], inplace=True)
+	elif source is 'ELO':
+		for df in dataset:
+			df.drop(df[(df.season < 2010) | (df.season >= 2019)].index, inplace=True)
+			df.fillna({'playoff': 'x'}, inplace=True)
+	return dataset
 
 
 def remove_carets(player_name):
 	return re.sub('[*+]', '', player_name)
-
