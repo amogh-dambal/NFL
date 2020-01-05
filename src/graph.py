e9065be4615bc090ecd0837302d33b62e191016e
Amogh Dambal <amdam2013@gmail.com>
2020-01-04 18:07:26 -0600

    updated graph for plotting central axes

--- src/graph.py
+++ src/graph.py
@@ -22,6 +22,17 @@ def graph(x, y, params=None):
 	if params is not None:
 		plt.title(f"{params['title']}")
 
+	plt.grid(True, color='black', linewidth=.1)
+	ax = plt.axes()
+
+	ax.spines['left'].set_position('zero')
+	ax.spines['right'].set_color('none')
+	ax.spines['bottom'].set_position('zero')
+	ax.spines['top'].set_color('none')
+	ax.spines['left'].set_smart_bounds(True)
+	ax.spines['bottom'].set_smart_bounds(True)
+	ax.xaxis.set_ticks_position('bottom')
+	ax.yaxis.set_ticks_position('left')
 	plt.show()
 
 
